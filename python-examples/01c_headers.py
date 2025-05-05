986a63dfa825ff70119933d23d699bed37e4ad5c
Wu-chang Feng <wuchang@pdx.edu>
2025-05-05 12:32:37 -0700

    fix naming

--- python-examples/01c_headers.py
+++ python-examples/01c_headers.py
@@ -1,7 +1,7 @@
 import requests
 s = requests.Session()
 url = 'http://mercury.picoctf.net:1270/'
-headers = {
+req_headers = {
   'User-Agent' : 'PicoBrowser',
   'Referer' : url,
   'Date' : 'Wed, 21 Oct 2018 00:00:00 GMT',
@@ -9,5 +9,5 @@ headers = {
   'X-Forwarded-For' : '2.16.66.0',
   'Accept-Language' : 'sv'
 }
-resp = s.get(url, headers=headers)
+resp = s.get(url, headers=req_headers)
 print(resp.text)
